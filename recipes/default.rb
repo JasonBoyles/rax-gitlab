f6f36a1396449301a6290b4b3f01083fbc37f6f2
Jason Boyles <jason@duncancreek.net>
2014-04-08 12:39:20 -0500

    initial commit

---
+++ recipes/default.rb
@@ -0,0 +1,48 @@
+include_recipe 'apt'
+include_recipe 'build-essential'
+
+include_recipe 'mysql::server'
+
+if node['gitlab']['generate_self_signed_cert']
+  if !File.exists?(node['gitlab']['ssl_certificate']) && !File.exists?(node['gitlab']['ssl_certificate_key'])
+    generate_self_signed_cert
+  end
+end
+
+include_recipe 'gitlab::default'
+include_recipe 'gitlab::mysql'
+
+gitlab_user_file = '/root/.gitlab.user.initialized'
+unless File.exists?(gitlab_user_file)
+
+  template node['gitlab']['app_home'] + '/user.rb' do
+    owner node['gitlab']['user']
+    group node['gitlab']['group']
+    mode '0600'
+    source 'user.rb.erb'
+    variables(
+      username: node['gitlab']['admin_user'],
+      password: node['gitlab']['admin_password'],
+      email: node['gitlab']['admin_email']
+    )
+  end
+
+  execute "Set Admin user Credentials" do
+    user node['gitlab']['user']
+    group node['gitlab']['group']
+    cwd node['gitlab']['app_home']
+    environment('LANG' => 'en_US.UTF-8', 'LC_ALL' => 'en_US.UTF-8')
+    command <<-EOS
+    ../bin/rails console production < user.rb
+    rm -f user.rb
+    EOS
+    action :run
+  end
+
+  execute "Touch User Initialized File" do
+    user 'root'
+    group 'root'
+    command "touch #{gitlab_user_file}"
+    action :run
+  end
+end
